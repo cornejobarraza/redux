9d970968cbb32917c91441ce22f9b792373cb341
David <davidcornejo1@live.com>
2022-09-12 21:58:21 -0600

    Added middleware and updated slice name

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/settings/settingsSlice";
 
+const settingsMiddleware = (store) => (next) => (action) => {
+  const result = next(action);
+  if (action.type.startsWith("user/")) {
+    const user = store.getState().settings;
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+  return result;
+};
+
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    settings: userReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(settingsMiddleware),
 });
