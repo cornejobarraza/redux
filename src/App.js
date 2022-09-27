3009f9facf083eed5c466f9d687800a2ee041f81
David <davidcornejo1@live.com>
2022-09-27 15:34:01 -0600

    Updated file to implement content container

--- src/App.js
+++ src/App.js
@@ -1,91 +1,18 @@
-import { useRef, useState, useEffect, Suspense, lazy } from "react";
-import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
-import { Navbar, Sidebar, SidebarToggle, SlideOver, ErrorBoundary, PrivateRoute, NotFound } from "components";
+import { useNavigate, useLocation } from "react-router-dom";
+import { Navbar, Sidebar, Content } from "components";
 import { history } from "helpers";
 
-const Login = lazy(() => import("pages/Login"));
-const Landing = lazy(() => import("pages/Landing"));
-const About = lazy(() => import("pages/About"));
-const Contact = lazy(() => import("pages/Contact"));
-const Lists = lazy(() => import("pages/Lists"));
-const Settings = lazy(() => import("pages/Settings"));
-
 function App() {
-  const [isSlideOverOpen, setIsSlideOverOpen] = useState(false);
-  const sidebarRef = useRef(null);
-
   // Init custom history object to allow navigation from
   // anywhere in the react app (inside or outside components)
   history.navigate = useNavigate();
   history.location = useLocation();
 
-  useEffect(() => {
-    if (window.innerWidth < 1024) {
-      setIsSlideOverOpen(false);
-    }
-    // eslint-disable-next-line
-  }, [history.location]);
-
-  const toggleSidebar = () => {
-    isSlideOverOpen ? setIsSlideOverOpen(false) : setIsSlideOverOpen(true);
-  };
-
   return (
     <div className="redux">
       <Navbar />
-      <Sidebar sidebarRef={sidebarRef} />
-      <div className="content">
-        <ErrorBoundary>
-          <Suspense fallback={<div className="md:text-center">Loading...</div>}>
-            <Routes>
-              <Route path="*" element={<NotFound />} />
-              <Route path="login" element={<Login />} />
-              <Route
-                path="/"
-                element={
-                  <PrivateRoute>
-                    <Landing />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="about"
-                element={
-                  <PrivateRoute>
-                    <About />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="contact"
-                element={
-                  <PrivateRoute>
-                    <Contact />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="lists"
-                element={
-                  <PrivateRoute>
-                    <Lists />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="settings"
-                element={
-                  <PrivateRoute>
-                    <Settings />
-                  </PrivateRoute>
-                }
-              />
-            </Routes>
-          </Suspense>
-        </ErrorBoundary>
-      </div>
-      <SidebarToggle handler={toggleSidebar} />
-      <SlideOver open={isSlideOverOpen} handler={toggleSidebar} />
+      <Sidebar />
+      <Content />
     </div>
   );
 }
