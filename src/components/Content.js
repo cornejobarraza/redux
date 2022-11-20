b607a96045b1ad0596c2bfe494a767436355820e
David <davidcornejo1@live.com>
2022-11-19 20:59:58 -0600

    Added a backdrop and animation for pending actions

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,34 @@
 import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import { ErrorBoundary, PrivateRoute, NotFound } from "components";
+import { ErrorBoundary, PrivateRoute } from "components";
+import { useSelector } from "react-redux";
+import Lottie from "lottie-react";
+import loading from "../loading.json";
 
 const Login = lazy(() => import("pages/Login"));
 const Landing = lazy(() => import("pages/Landing"));
 const About = lazy(() => import("pages/About"));
 const Contact = lazy(() => import("pages/Contact"));
-const Lists = lazy(() => import("pages/Lists"));
+const Wishlist = lazy(() => import("pages/Wishlist"));
 const Settings = lazy(() => import("pages/Settings"));
 
 export { Content };
 
 function Content() {
+  const { pending } = useSelector((state) => state.user);
+
   return (
     <div className="content">
+      {(pending.login || pending.update || pending.logout) && (
+        <>
+          <div className="backdrop"></div>
+          <Lottie className="lottie" animationData={loading} />
+        </>
+      )}
       <ErrorBoundary>
         <Suspense fallback={<div className="md:text-center">Loading...</div>}>
           <Routes>
-            <Route path="*" element={<NotFound />} />
+            <Route path="*" element={<div className="md:text-center">Sorry, this page doesn't exist :(</div>} />
             <Route path="login" element={<Login />} />
             <Route
               path="/"
@@ -44,10 +55,10 @@ function Content() {
               }
             />
             <Route
-              path="lists"
+              path="wishlist"
               element={
                 <PrivateRoute>
-                  <Lists />
+                  <Wishlist />
                 </PrivateRoute>
               }
             />
