c40622bfee8b2b9d6aa3d9dfef232d6a8c601e89
David <davidcornejo1@live.com>
2023-05-11 15:12:01 -0600

    Add user name to first feature item

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,13 +1,72 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { IdentificationIcon, CodeBracketIcon, UserIcon, Cog6ToothIcon } from "@heroicons/react/24/outline";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import { IdentificationIcon, CodeBracketIcon, UserIcon, Cog6ToothIcon } from "@heroicons/react/24/outline";
 
 export { Landing as default };
 
 function Landing() {
+  const { user } = useSelector((state) => state.auth);
+
   useEffect(() => {
     document.title = "Redux - Main";
   }, []);
+
+  const features = [
+    {
+      name: `1. User data is shared across components ${user?.name.split(" ")[0]}`,
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione",
+      icon: UserIcon,
+    },
+    {
+      name: "2. Go to settings on the sidebar or click the user avatar",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione",
+      icon: Cog6ToothIcon,
+    },
+    {
+      name: "3. Update anything and watch the magic happen!",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione",
+      icon: IdentificationIcon,
+    },
+    {
+      name: (
+        <span>
+          4. Built using{" "}
+          <a
+            href="https://react-redux.js.org"
+            target="_blank"
+            rel="noreferrer"
+            className="text-redux-500 hover:text-redux-400"
+          >
+            React Redux,
+          </a>{" "}
+          <a
+            href="https://firebase.google.com"
+            target="_blank"
+            rel="noreferrer"
+            className="text-[#F57C00] hover:text-[#FFA000]"
+          >
+            Firebase
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://tailwindcss.com"
+            target="_blank"
+            rel="noreferrer"
+            className="text-sky-600 hover:text-sky-500"
+          >
+            Tailwind CSS
+          </a>
+        </span>
+      ),
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione",
+      icon: CodeBracketIcon,
+    },
+  ];
 
   return (
     <div className="main">
@@ -34,54 +93,3 @@ function Landing() {
     </div>
   );
 }
-
-const features = [
-  {
-    name: "1. User data is shared across components here",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione",
-    icon: UserIcon,
-  },
-  {
-    name: "2. Go to settings on the sidebar or click the user avatar",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione",
-    icon: Cog6ToothIcon,
-  },
-  {
-    name: "3. Update anything and watch the magic happen!",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione",
-    icon: IdentificationIcon,
-  },
-  {
-    name: (
-      <span>
-        4. Built using{" "}
-        <a
-          href="https://react-redux.js.org"
-          target="_blank"
-          rel="noreferrer"
-          className="text-redux-500 hover:text-redux-400"
-        >
-          React Redux,
-        </a>{" "}
-        <a
-          href="https://firebase.google.com"
-          target="_blank"
-          rel="noreferrer"
-          className="text-[#F57C00] hover:text-[#FFA000]"
-        >
-          Firebase
-        </a>{" "}
-        and{" "}
-        <a href="https://tailwindcss.com" target="_blank" rel="noreferrer" className="text-sky-600 hover:text-sky-500">
-          Tailwind CSS
-        </a>
-      </span>
-    ),
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione",
-    icon: CodeBracketIcon,
-  },
-];
