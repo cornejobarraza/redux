82a4accf7a87160d80e687459086e28e42187d56
David <davidcornejo1@live.com>
2023-05-14 00:22:21 -0600

    Move handler function inside useEffect

--- src/components/ViewportProvider/index.js
+++ src/components/ViewportProvider/index.js
@@ -8,12 +8,12 @@ function ViewportProvider({ children }) {
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
 
-  const handleWindowResize = () => {
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight);
-  };
-
   useEffect(() => {
+    const handleWindowResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
     window.addEventListener("resize", handleWindowResize);
     return () => window.removeEventListener("resize", handleWindowResize);
   }, []);
