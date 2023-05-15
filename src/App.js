6ccb24b4a500bf4976e0f069fbb08f33c50d5e07
David <davidcornejo1@live.com>
2023-05-15 10:52:17 -0600

    Add react-idle-timer to manage user inactivity

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import { useNavigate, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useIdleTimer } from "react-idle-timer";
 import { ToastContainer } from "react-toastify";
 
 import { Navbar, Sidebar, Content, ViewportProvider } from "components";
 
+import { userActions } from "store";
 import { history } from "utils";
 
 import "react-toastify/dist/ReactToastify.min.css";
 
+// Convert 30 minutes to milliseconds
+const idleTimeout = 30 * 60_000;
+
 function App() {
+  const {
+    logged: { status: logged },
+  } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
   // Init custom history object to allow navigation from
   // anywhere in the React app (inside or outside components)
   history.navigate = useNavigate();
   history.location = useLocation();
+
+  const idleLogOut = () => {
+    if (logged) {
+      dispatch(userActions.logOutAsync());
+    }
+  };
+
+  useIdleTimer({
+    onIdle: idleLogOut,
+    timeout: idleTimeout,
+  });
 
   return (
     <div className="redux">
